a2044fea2ca93a0d855ed7052c9d24775af0f6ff
hj hubeek <hjhubeek@gmail.com>
2020-10-03 11:05:01 +0200

    added Linkservice plus test add baseurl prop as app.prop

--- src/main/java/nl/appall/urlshortener/LinkService.java
+++ src/main/java/nl/appall/urlshortener/LinkService.java
@@ -1,14 +1,22 @@
 package nl.appall.urlshortener;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class LinkService {
+
+    private  final String baseUrl;
+
+    public LinkService(@Value("${app.baseUrl}") String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
     Mono<String> shortenLink (String link){
         String randomKey = RandomStringUtils.randomAlphabetic(6);
 // save to db
-        return Mono.just(randomKey);
+        return Mono.just(baseUrl + randomKey);
     }
 }
---
+++ src/test/java/nl/appall/urlshortener/LinkServiceTest.java
@@ -0,0 +1,23 @@
+package nl.appall.urlshortener;
+
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+public class LinkServiceTest {
+
+    private LinkService linkService = new LinkService("http://some-domain.com/");
+
+    @Test
+    public void shortensLink() {
+        StepVerifier.create(linkService.shortenLink("http://spring.io"))
+        .expectNextMatches(result ->
+                result != null
+                && result.length() > 0
+                && result.startsWith("http://some-domain.com/")
+        )
+        .expectComplete()
+        .verify();
+
+    }
+
+}

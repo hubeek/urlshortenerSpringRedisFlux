b80c249a516d3bf76eec9747ca14958815b93072
HJHubeek <hjhubeek@gmail.com>
2020-10-05 19:53:14 +0200

    updated faulty test

--- src/test/java/nl/appall/urlshortener/LinkControllerTest.java
+++ src/test/java/nl/appall/urlshortener/LinkControllerTest.java
@@ -25,7 +25,7 @@ public class LinkControllerTest {
     private LinkService linkService;
 
     @Test
-    public void shortensLink(){
+    public void shortensLink() {
         when(linkService.shortenLink("https://spring.io")).thenReturn(Mono.just("http://localhost:8080/aass2211"));
         webTestClient.post()
                 .uri("/link")
@@ -36,18 +36,19 @@ public class LinkControllerTest {
                 .is2xxSuccessful()
                 .expectBody()
                 .jsonPath("$.shortenedLink")
-                     .value(val -> assertThat(val).isEqualTo("http://localhost:8080/aass2211"));
+                .value(val -> assertThat(val).isEqualTo("http://localhost:8080/aass2211"));
     }
+
     @Test
     public void redirectsToOriginalLink() {
         when(linkService.getOriginalLink("aaa21123"))
                 .thenReturn(Mono.just(new Link("http://sprint.io", "aaa21123")));
         webTestClient.get()
-                     .uri("/aaa21123")
-                     .exchange()
-                     .expectStatus()
-                     .isPermanentRedirect()
-                     .expectHeader()
-                     .value("Location", location -> assertThat(location).isEqualTo("http://sprint.io"));
+                .uri("/aaa21123")
+                .exchange()
+                .expectStatus()
+                .isPermanentRedirect()
+                .expectHeader()
+                .value("Location", location -> assertThat(location).isEqualTo("http://sprint.io"));
     }
 }

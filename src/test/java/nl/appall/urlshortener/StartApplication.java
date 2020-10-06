fc2879aac02720b43ae5aa2a9d5ebb8ae5b29860
HJHubeek <hjhubeek@gmail.com>
2020-10-06 08:59:39 +0200

    start from test to create automatic redis instance.

---
+++ src/test/java/nl/appall/urlshortener/StartApplication.java
@@ -0,0 +1,9 @@
+package nl.appall.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlshortenerApplication.class, args);
+    }
+}
